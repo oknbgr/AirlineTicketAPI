ffde98dbd5d6508157faaf435fbade062ef3662b
oknbgr <okanbilgir@hotmail.com>
2023-03-20 14:32:34 +0300

    initial commit

---
+++ src/main/kotlin/com/example/AirlineTicketAPI/AirlineTicketApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.AirlineTicketAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirlineTicketApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirlineTicketApiApplication>(*args)
+}
