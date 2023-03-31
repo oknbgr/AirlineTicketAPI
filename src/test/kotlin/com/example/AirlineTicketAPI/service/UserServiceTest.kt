c553c6cdb7b0791bb02e9f1773eff6aa9542b4b4
Okan <93092104+oknbgr@users.noreply.github.com>
2023-03-31 11:38:31 +0300

    Deleted tests from old implementation methods.

--- src/test/kotlin/com/example/AirlineTicketAPI/service/UserServiceTest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.AirlineTicketAPI.service
-
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-@AutoConfigureMockMvc
-internal class UserServiceTest {
-}
